492cbe8ccc31c7ccd0c0d906c2b32ca9bf547ee7
Kryz14 <ctechlanre@gmail.com>
2024-10-29 21:39:12 -0700

    Update dependencies and main.jsx for React 17 compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom'; // Import ReactDOM from 'react-dom'
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+  document.getElementById('root') // Make sure this matches your HTML file's root element ID
+);
